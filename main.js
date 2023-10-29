fec19a0bb37ebfa9497e6f7fe9c68c504819731a
AmanuelTilahun <amanuel.tilahun98@gmail.com>
2023-10-29 00:45:02 -0400

    ADD UI

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        return "Rock";
-    }
-    else if (randomNumber === 2) {
-        return "Paper";
-    }
-    else if (randomNumber === 3) {
-        return "Scissor";
-    }
-    else
-        return "something's wrong";
+    if (randomNumber === 1) return "Rock";
+    else if (randomNumber === 2) return "Paper";
+    else if (randomNumber === 3) return "Scissor";
+    else return "something's wrong";
 }
 
 let playerScore = 0;
@@ -22,37 +15,68 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "No one wins";
+        return "No one wins. player score: " + playerScore + " computer score: " + computerScore;
     }
     else if (playerSelection === "rock" && computerSelection === "scissor") {
         playerScore += 1;
-        return "You win, rock beats scissor";
+        return "You win, rock beats scissor. player score: " + playerScore + " computer score: " + computerScore;
     }
     else if (playerSelection === "scissor" && computerSelection === "paper") {
         playerScore += 1;
-        return "You win, scissor beats paper";
+        return "You win, scissor beats paper. player score: " + playerScore + " computer score: " + computerScore;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore += 1;
-        return "You win, paper beats rock";
+        return "You win, paper beats rock. player score: " + playerScore + " computer score: " + computerScore;
     }
     else {
         computerScore += 1;
-        return "You lose," + computerSelection + " beats " + playerSelection;
+        return "You lose, " + computerSelection + " beats " + playerSelection + ". player score: " + playerScore + " computer score: " + computerScore;
     }
 }
 
+const body = document.querySelector('#body');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const buttons = document.querySelector('#buttons');
+
+const result = document.createElement('div');
+body.appendChild(result);
+const finalResult = document.createElement('div');
+body.appendChild(finalResult);
+
+const gamesPlayed = document.createElement('div');
+body.appendChild(gamesPlayed);
+
 function game() {
-    let computerChoice;
-    for (i = 0; i < 5; i++) {
+    let playerSelection = '';
+    let computerChoice = '';
+    let numberOfGames = 0;
+    buttons.addEventListener('click', (event) => {
+        let target = event.target;
+        switch (target.id) {
+            case 'rock':
+                playerSelection = 'rock';
+                break;
+            case 'paper':
+                playerSelection = 'paper';
+                break;
+            case 'scissor':
+                playerSelection = 'scissor';
+                break;
+        }
         computerChoice = getComputerChoice();
-        const playerChoice = prompt("Choose Rock, Paper, or Scissor", " ");
-        playRound(playerChoice, computerChoice);
-        console.log("player choice:" + playerChoice + "\n computer choice:" + computerChoice + "\n your score: " + playerScore + "\n copmuter score: " + computerScore);
-    }
-    return (playerScore > computerScore) ? "You win, score: " + playerScore : "You lose, score: " + computerScore;
+        result.textContent = playRound(playerSelection, computerChoice);
+        gamesPlayed.textContent = "number of Games: " + numberOfGames;
+        if (playerScore + computerScore === 5) {
+            (playerScore > computerScore) ? finalResult.textContent = "final result: You win, score: " + playerScore : finalResult.textContent = "final result: You lose, score: " + computerScore;
+            numberOfGames += 1;
+            playerScore = 0;
+            computerScore = 0;
+        }
+    });
+
 }
 
 game();
-console.log(game());
-
